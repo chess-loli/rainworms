a6ebdf5c388f5935d62bc54636ee4216504e85bb
YoZen <yrck666@gmail.com>
2019-08-15 15:59:01 +0200

    optimization + started grocery list

--- javascript in html Vue/main.js
+++ javascript in html Vue/main.js
@@ -1,11 +1,41 @@
-Vue.component('diceTable', {
-    template: '<table> <tbody> <tr> </tr> <tr> </tr> <tr> </tr> <tr> </tr> <tr> </tr> </tbody> </table>',
-    
-    data() {
-        return {
-            dice: []
-        }
+Vue.component('dice-table', {
+    props: {
+        dice: Array,
     },
+    template: ` 
+                <div style="margin-top: 40px">
+                    <table class="table is-bordered"> 
+                        <tbody> 
+                            <tr>
+                                <th>dice number</th>
+                                <td>amount of them rolled</td>
+                            </tr>
+                            <tr v-for="(amount, die) in diceCounter">
+                                <th>{{die}}</th>
+                                <td>{{amount}}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            `,
+
+    
+computed: {
+    diceCounter() {
+        return this.dice.reduce((counter, dice) => {
+            counter[dice]++
+            return counter
+        }, {
+            1:0,
+            2:0,
+            3:0,
+            4:0,
+            5:0,
+            6:0
+        })
+    }
+},
+    
 
 
 });
@@ -14,4 +44,31 @@ Vue.component('diceTable', {
 new Vue({
     el: '#root',
     
+    data: {
+        dice: []
+    },
+
+    methods: {
+        reset() {
+            if (this.dice.length > 2) {
+                this.dice = []
+            }
+           
+        },
+        throwDice() {
+            this.reset()
+            this.oneIsEight()
+            this.dice.sort()
+        },
+        oneIsEight() {
+            this.dice.push(Math.floor(Math.random() * 6) + 1)
+            this.dice.push(Math.floor(Math.random() * 6) + 1)
+            this.dice.push(Math.floor(Math.random() * 6) + 1)
+            this.dice.push(Math.floor(Math.random() * 6) + 1)
+            this.dice.push(Math.floor(Math.random() * 6) + 1)
+            this.dice.push(Math.floor(Math.random() * 6) + 1)
+            this.dice.push(Math.floor(Math.random() * 6) + 1)
+            this.dice.push(Math.floor(Math.random() * 6) + 1)
+        },
+    }
 });
